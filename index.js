f47dbbc362afa689487e2560dfba48317eab6929
Kateryna Kalinina <katerynakalinina7@gmail.com>
2020-10-06 17:45:42 +0300

    homework20

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const { read, write } = require("./utils");
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  const messages = read("messages");
+
+  switch (request.url) {
+    case "/add":
+      const newData = [
+        ...messages,
+        {
+          id: messages[messages.length - 1].id + 1,
+          name: "No-name",
+        },
+      ];
+
+      write("messages", newData);
+      break;
+
+    case "/delete":
+
+      write("messages", messages.splice(messages.findIndex((item, id) => item.id === id), 1));
+      break;
+
+  }
+
+  response.end(JSON.stringify(read("messages")));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is listening ${hostname}:${port}`);
+});
