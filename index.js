3a89301d66dbaff248f29492b8535c445eae096a
Kateryna Kalinina <katerynakalinina7@gmail.com>
2020-10-08 02:33:24 +0300

    deleteWithId

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require("http");
+const url = require("url");
+const queryString = require("querystring");
 const { read, write } = require("./utils");
 
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const { id } = queryString.parse(parsedUrl.query);
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
   const messages = read("messages");
 
-  switch (request.url) {
+  switch (parsedUrl.pathname) {
     case "/add":
       const newData = [
         ...messages,
@@ -20,10 +27,19 @@ const server = http.createServer((request, response) => {
       write("messages", newData);
       break;
 
-      case "/delete":
+    case "/delete":
+      const newArray = [...messages];
+      if (id) {
+        const elementIndex = newArray.findIndex(el => el.id === Number(id));
 
-        write("messages", messages.slice(0, messages.length - 1));
-        break;
+        if (elementIndex !== -1) {
+          newArray.splice(elementIndex, 1);
+        }
+      } else {
+        newArray.splice(newArray.length - 1, 1);
+      }
+
+      write("messages");
   }
 
   response.end(JSON.stringify(read("messages")));
